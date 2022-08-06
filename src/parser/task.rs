f1ef3c99da042e084a7953a3bdc85c843250655b
Antti Peltonen <antti.peltonen@solita.fi>
2022-08-06 12:12:28 +0300

    Task to/from yaml string

--- src/parser/task.rs
+++ src/parser/task.rs
@@ -1,6 +1,6 @@
 use super::lexicon::{Expression, parse};
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use serde::{Serialize, Deserialize};
 use thiserror::Error;
@@ -23,10 +23,17 @@ pub struct Task {
     pub description: String,
     pub project: Option<String>,
     pub tags: Option<Vec<String>>,
-    pub metadata: Option<HashMap<String, String>>,
+    pub metadata: Option<BTreeMap<String, String>>,
 }
 
 impl Task {
+    pub fn new(description: String) -> Self {
+        let timestamp = chrono::offset::Utc::now();
+        let mut metadata: BTreeMap<String, String> = BTreeMap::new();
+        metadata.insert(String::from("tsk-rs-task-create-time"), timestamp.to_rfc3339());
+        Self { id: Uuid::new_v4(), description: description, project: None, tags: None, metadata: Some(metadata) }
+    }
+
     pub fn to_yaml_string(&self) -> Result<String> {       
         let yaml = serde_yaml::to_string(self)?;
         Ok(yaml)
@@ -37,12 +44,12 @@ impl Task {
         return Ok(task)
     }
 
-    pub fn parse_task_descriptor(input: &'static str) -> Result<Self> {
+    pub fn from_task_descriptor(input: &'static str) -> Result<Self> {
         let (_, expressions) = parse(input)?;
 
         let mut description: String = String::new();
         let mut tags: Vec<String> = vec![];
-        let mut metadata: HashMap<String, String> = HashMap::new();
+        let mut metadata: BTreeMap<String, String> = BTreeMap::new();
         let mut project: String = String::new();
 
         for expr in expressions {
@@ -84,25 +91,23 @@ impl Task {
             };
         }
 
-        // restruct tags and metadata into options for the constructor
         let mut ret_tags = None;
         if tags.len() > 0 {
             ret_tags = Some(tags)
-        }
-        let mut ret_metadata = None;
-        if metadata.len() > 0 {
-            ret_metadata = Some(metadata)
         }
         let mut ret_project = None;
         if !project.is_empty() {
             ret_project = Some(project);
         }
 
+        let timestamp = chrono::offset::Utc::now();
+        metadata.insert(String::from("tsk-rs-task-create-time"), timestamp.to_rfc3339());
+
         Ok(Self {
             id: Uuid::new_v4(),
             description: description,
             tags: ret_tags,
-            metadata: ret_metadata,
+            metadata: Some(metadata),
             project: ret_project,
         })
     }
@@ -110,6 +115,8 @@ impl Task {
 
 #[cfg(test)]
 mod tests {
+    use chrono::{DateTime, Datelike};
+
     use super::*;
 
     static FULLTESTCASEINPUT: &str = "some task description here @project-here #taghere #a-second-tag %x-meta:data %x-fuu:bar additional text at the end";
@@ -117,7 +124,7 @@ mod tests {
     static MULTIPROJECTINPUT: &str = "this has a @project-name, and a @second-project name";
     static DUPLICATEMETADATAINPUT: &str = "this has %x-fuu:bar definied again with %x-fuu:bar";
     static INVALIDMETADATAKEY: &str = "here is an %invalid:metadata key";
-    static YAMLTESTINPUT: &str = "id: bd6f75aa-8c8d-47fb-b905-d9f7b15c782d\ndescription: some task description here additional text at the end\nproject: project-here\ntags:\n- taghere\n- a-second-tag\nmetadata:\n  x-meta: data\n  x-fuu: bar\n";
+    static YAMLTESTINPUT: &str = "id: bd6f75aa-8c8d-47fb-b905-d9f7b15c782d\ndescription: some task description here additional text at the end\nproject: project-here\ntags:\n- taghere\n- a-second-tag\nmetadata:\n  x-meta: data\n  x-fuu: bar\n  x-meta: data\n  tsk-rs-task-create-time: 2022-08-06T07:55:26.568460389+00:00\n";
 
     #[test]
     fn test_from_yaml() {
@@ -128,11 +135,16 @@ mod tests {
         assert_eq!(task.tags, Some(vec![String::from("taghere"), String::from("a-second-tag")]));
         assert_eq!(task.metadata.clone().unwrap().get("x-meta"), Some(&String::from("data")));
         assert_eq!(task.metadata.clone().unwrap().get("x-fuu"), Some(&String::from("bar")));
+
+        let timestamp = DateTime::parse_from_rfc3339(task.metadata.clone().unwrap().get("tsk-rs-task-create-time").unwrap()).unwrap();
+        assert_eq!(timestamp.year(), 2022);
+        assert_eq!(timestamp.month(), 8);
+        assert_eq!(timestamp.day(), 6);
     }
 
     #[test]
     fn test_to_yaml() {
-        let mut task = Task::parse_task_descriptor(FULLTESTCASEINPUT).unwrap();
+        let mut task = Task::from_task_descriptor(FULLTESTCASEINPUT).unwrap();
 
         // for testing we need to know the UUID so create a new one and override autoassigned one
         let test_uuid = Uuid::parse_str("bd6f75aa-8c8d-47fb-b905-d9f7b15c782d").unwrap();
@@ -140,12 +152,13 @@ mod tests {
 
         let yaml_string = task.to_yaml_string().unwrap();
         assert_eq!(yaml_string,
-            format!("id: {}\ndescription: {}\nproject: {}\ntags:\n- {}\n- {}\nmetadata:\n  x-fuu: {}\n  x-meta: {}\n",
+            format!("id: {}\ndescription: {}\nproject: {}\ntags:\n- {}\n- {}\nmetadata:\n  tsk-rs-task-create-time: {}\n  x-fuu: {}\n  x-meta: {}\n",
                 task.id,
                 task.description,
                 task.project.unwrap(),
                 task.tags.clone().unwrap().get(0).unwrap(),
                 task.tags.clone().unwrap().get(1).unwrap(),
+                task.metadata.clone().unwrap().get("tsk-rs-task-create-time").unwrap(),
                 task.metadata.clone().unwrap().get("x-fuu").unwrap(),
                 task.metadata.clone().unwrap().get("x-meta").unwrap(),
             ));
@@ -153,7 +166,7 @@ mod tests {
 
     #[test]
     fn parse_full_testcase() {
-        let task = Task::parse_task_descriptor(FULLTESTCASEINPUT).unwrap();
+        let task = Task::from_task_descriptor(FULLTESTCASEINPUT).unwrap();
 
         assert_eq!(task.project, Some(String::from("project-here")));
         assert_eq!(task.description, "some task description here additional text at the end");
@@ -164,31 +177,32 @@ mod tests {
 
     #[test]
     fn parse_no_expressions() {
-        let task = Task::parse_task_descriptor(NOEXPRESSIONSINPUT).unwrap();
+        let task = Task::from_task_descriptor(NOEXPRESSIONSINPUT).unwrap();
 
         assert_eq!(task.project, None);
         assert_eq!(task.description, NOEXPRESSIONSINPUT);
         assert_eq!(task.tags, None);
-        assert_eq!(task.metadata, None);
+
+        assert!(task.metadata.clone().unwrap().get("tsk-rs-task-create-time").is_some());
     }
 
     #[test]
     fn reject_multiple_projects() {
-        let task = Task::parse_task_descriptor(MULTIPROJECTINPUT);
+        let task = Task::from_task_descriptor(MULTIPROJECTINPUT);
 
         assert_eq!(task.unwrap_err().downcast::<TaskError>().unwrap(), TaskError::MultipleProjectsNotAllowed);
     }
 
     #[test]
     fn reject_duplicate_metadata() {
-        let task = Task::parse_task_descriptor(DUPLICATEMETADATAINPUT);
+        let task = Task::from_task_descriptor(DUPLICATEMETADATAINPUT);
 
         assert_eq!(task.unwrap_err().downcast::<TaskError>().unwrap(), TaskError::IdenticalMetadataKeyNotAllowed(String::from("x-fuu")));
     }
 
     #[test]
     fn require_metadata_prefix() {
-        let task = Task::parse_task_descriptor(INVALIDMETADATAKEY);
+        let task = Task::from_task_descriptor(INVALIDMETADATAKEY);
 
         assert_eq!(task.unwrap_err().downcast::<TaskError>().unwrap(), TaskError::MetadataPrefixInvalid(String::from("invalid")));
     }
