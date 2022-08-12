70d53addd9f89c7d89c68c75336e11a9caf0dbd2
Antti Peltonen <antti.peltonen@solita.fi>
2022-08-12 13:24:59 +0300

    basic task note manipulation

--- src/bin/tsknt.rs
+++ src/bin/tsknt.rs
@@ -1,10 +1,13 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, fs::remove_file};
 
 use anyhow::{Result, Context, bail};
 use clap::{Parser, Subcommand};
+use cli_table::{Cell, Table, Style, format::Border, print_stdout};
 use config::Config;
+use question::{Question, Answer};
 use tsk_rs::{settings::Settings, task::{Task, TaskError}, note::Note};
 use edit::edit;
+use glob::glob;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -29,6 +32,26 @@ enum Commands {
         #[clap(short, long, value_parser)]
         raw: bool,
     },
+    /// delete a note file
+    Delete {
+        /// task id
+        #[clap(value_parser)]
+        id: String,
+        /// delete file silently
+        #[clap(short, long, value_parser)]
+        force: bool,
+    },
+    Show {
+        /// task id
+        #[clap(value_parser)]
+        id: Option<String>,
+        /// show orphaned notes (task file has been deleted)
+        #[clap(short, long, value_parser)]
+        orphaned: bool,
+        /// show notes for completed tasks
+        #[clap(short, long, value_parser)]
+        completed: bool,
+    },
 }
 
 fn main() -> Result<()> {
@@ -44,9 +67,90 @@ fn main() -> Result<()> {
         Some(Commands::Jot { id, raw }) => {
             jot_note(id, raw, &settings)
         },
-        None => {todo!("we should list available notes here"); }
+        Some(Commands::Show {id, orphaned, completed }) => {
+            show_note(id, orphaned, completed, &settings)
+        },
+        Some(Commands::Delete { id, force }) => {
+            delete_note(id, force, &settings)
+        }
+        None => { show_note(&None, &false, &false, &settings) }
     }
 }
+
+fn show_note(id: &Option<String>, orphaned: &bool, completed: &bool, settings: &Settings) -> Result<()> {
+    let mut note_cells = vec![];
+
+    let mut note_pathbuf: PathBuf = settings.note_db_pathbuf().with_context(|| {"invalid data directory path configured"})?;
+    if id.is_some() {
+        note_pathbuf = note_pathbuf.join(format!("*{}*.yaml", id.as_ref().unwrap()));
+    } else {
+        note_pathbuf = note_pathbuf.join("*.yaml");
+    }
+    for note_filename in glob(note_pathbuf.to_str().unwrap()).with_context(|| {"while traversing note data directory files"})? {
+        let note = Note::load_yaml_file_from(&note_filename?).with_context(|| {"while loading note from disk"})?;
+
+        let task_pathbuf = settings.task_db_pathbuf()?.join(PathBuf::from(format!("{}.yaml", note.task_id)));
+        let mut task: Option<Task> = None;
+        if task_pathbuf.is_file() {
+            task = Some(Task::load_yaml_file_from(&task_pathbuf).with_context(|| {"while loading task from yaml file"})?);
+        }
+
+        if let Some(task) = task {
+            let mut show_note = false;
+            // there is a task file
+            if task.is_done() && *completed {
+                // .. but the task is completed. however completed is true so we show it
+                show_note = true;
+            }
+            if !task.is_done() {
+                // .. task is not done so show it
+                show_note = true;
+            }
+            if show_note {
+                note_cells.push(vec![note.task_id.cell(), task.description.cell(),
+                    task.project.unwrap_or_else(|| {"".to_string()}).cell(),]);
+            }
+        } else if *orphaned {
+            // there is no task file anymore, and orphaned is true so we add it
+            note_cells.push(vec![
+                note.task_id.cell(),
+                "[orphaned]".to_string().cell(),
+                "[orphaned]".to_string().cell(),
+                "[orphaned]".to_string().cell(),
+            ]);
+        }
+    }
+
+    if !note_cells.is_empty() {
+        let tasks_table = note_cells.table()
+            .title(
+                vec!["ID".cell().bold(true),
+                "Description".cell().bold(true),
+                "Project".cell().bold(true)]) // headers of the table
+            .border(Border::builder().build()); // empty border around the table
+        print_stdout(tasks_table).with_context(|| {"while trying to print out pretty table of task(s)"})?;
+    } else {
+        println!("No task notes");
+    }
+
+    Ok(())
+}
+
+fn delete_note(id: &String, force: &bool, settings: &Settings) -> Result<()> {
+    let note_pathbuf = settings.note_db_pathbuf()?.join(PathBuf::from(format!("{}.yaml", id)));
+
+    let answer = Question::new("Really delete this note?")
+        .default(Answer::NO)
+        .show_defaults()
+        .confirm();
+
+    if answer == Answer::YES || *force {
+        remove_file(note_pathbuf).with_context(|| {"while removing note file"})?;
+    }
+
+    Ok(())
+}
+
 
 fn jot_note(id: &String, raw: &bool, settings: &Settings) -> Result<()> {
     let task_pathbuf = settings.task_db_pathbuf()?.join(PathBuf::from(format!("{}.yaml", id)));
