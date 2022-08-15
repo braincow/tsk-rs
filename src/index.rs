6596388c6ba1bbff50fa79dcfec9be4fcbbd78cc
Antti Peltonen <antti.peltonen@solita.fi>
2022-08-15 16:41:40 +0300

    ID needs to be stored in index for referals

--- src/index.rs
+++ src/index.rs
@@ -2,7 +2,7 @@ use tantivy::{schema::{Schema, TEXT, STORED}};
 
 pub fn task_schema() -> Schema {
     let mut schema_builder = Schema::builder();
-    schema_builder.add_text_field("ID", TEXT);
+    schema_builder.add_text_field("ID", TEXT | STORED);
     schema_builder.add_text_field("description", TEXT | STORED);
     schema_builder.add_text_field("project", TEXT);
     schema_builder.build()
@@ -10,8 +10,8 @@ pub fn task_schema() -> Schema {
 
 pub fn note_schema() -> Schema {
     let mut schema_builder = Schema::builder();
-    schema_builder.add_text_field("ID", TEXT);
-    schema_builder.add_text_field("markdown", TEXT | STORED);
+    schema_builder.add_text_field("ID", TEXT | STORED);
+    schema_builder.add_text_field("markdown", TEXT);
     schema_builder.build()
 }
 
