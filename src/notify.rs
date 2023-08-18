67f803e4a36f2699deefe4b74563db42dbd565ef
Antti Peltonen <antti.peltonen@solita.fi>
2023-08-18 15:27:28 +0300

    improved the watcher

--- src/notify.rs
+++ src/notify.rs
@@ -1,10 +1,20 @@
-use notify::{RecursiveMode, recommended_watcher, Watcher, Event};
+use notify::{RecursiveMode, recommended_watcher, Watcher};
 use std::sync::mpsc;
 use std::thread;
 use crate::settings::Settings;
+use regex::Regex;
+
+/// Which type of a file was modified
+#[derive(Debug)]
+pub enum DatabaseFileType {
+    /// Modified file was a Task file. Enum contains filename and path.
+    Task(String),
+    /// Modified file was a Note file. Enum contains filename and path.
+    Note(String)
+}
 
 /// Function type for change callback.
-pub type ChangeCallback = fn(Event);
+pub type ChangeCallback = fn(DatabaseFileType);
 
 /// Function type for error callback.
 pub type ErrorCallback = fn(String);
@@ -27,7 +37,12 @@ impl FilesystemMonitor {
         on_change: ChangeCallback,
         on_error: ErrorCallback,
     ) {
-        let path = settings.as_ref().db_pathbuf().unwrap();
+        let re = Regex::new(r"^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}\.yaml$").unwrap(); // TODO: fix unwrap
+        let path = settings.as_ref().db_pathbuf().unwrap(); // TODO: fix unwrap
+        let task_db_path = settings.as_ref().task_db_pathbuf().unwrap(); // TODO: fix unwrap
+        let task_db_path_str = task_db_path.to_str().unwrap().to_string();  // TODO: fix unwrap
+        let note_db_path = settings.as_ref().note_db_pathbuf().unwrap();  // TODO: fix unwrap
+        let note_db_path_str = note_db_path.to_str().unwrap().to_string();  // TODO: fix unwrap
 
         // Spawn a new thread to monitor the filesystem.
         self.watcher_thread = Some(thread::spawn(move || {
@@ -47,7 +62,27 @@ impl FilesystemMonitor {
                 match rx.recv() {
                     Ok(event) => {
                         match event {
-                            Ok(event) => on_change(event),
+                            Ok(event) => {
+                                for path in event.paths {
+                                    if !path.is_file() {
+                                        // not a file, loop to next iteration
+                                        continue;
+                                    }
+                                    let filename_string = path.file_name().unwrap().to_str().unwrap();
+                                    let pathname_string = path.parent().unwrap().to_str().unwrap();
+                                    // only act if the change is for a yaml file
+                                    if re.is_match(filename_string) {
+                                        // then try to match the path of the db file to subpath to determine the type
+                                        if pathname_string == task_db_path_str {
+                                            on_change(DatabaseFileType::Task(filename_string.to_string()));
+                                        } else if pathname_string == note_db_path_str {
+                                            on_change(DatabaseFileType::Note(filename_string.to_string()));
+                                        } else {
+                                            on_error(format!("file changed in flatfile database, but its neither a Task or a Note: {filename_string}"));
+                                        }
+                                    }
+                                }
+                            },
                             Err(e) => on_error(format!("Event error: {:?}", e))
                         };
                     }
@@ -68,3 +103,5 @@ impl Drop for FilesystemMonitor {
         }
     }
 }
+
+//  eof
